b251bd79ff5be1a0101125e972fcd76e0bb92af7
4simb <63647269+4simb@users.noreply.github.com>
2022-09-16 21:37:41 +0300

    Add project files.

---
+++ BMP.h
@@ -0,0 +1,162 @@
+// CREDITS: 
+// https://solarianprogrammer.com/2018/11/19/cpp-reading-writing-bmp-images/
+// https://github.com/sol-prog/cpp-bmp-images
+
+#pragma once
+#include <fstream>
+#include <stdexcept>
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+
+// bytes per raw = (x_size + 31) / 32
+
+#pragma pack(push, 1)
+struct BMPFileHeader {
+    uint16_t file_type{ 0x4D42 };          // File type always BM which is 0x4D42 (stored as hex uint16_t in little endian)
+    uint32_t file_size{ 0 };               // Size of the file (in bytes)
+    uint16_t reserved1{ 0 };               // Reserved, always 0
+    uint16_t reserved2{ 0 };               // Reserved, always 0
+    uint32_t offset_data{ 0 };             // Start position of pixel data (bytes from the beginning of the file)
+};
+
+struct BMPInfoHeader {
+    uint32_t size{ 0 };                      // Size of this header (in bytes)
+    int32_t width{ 0 };                      // width of bitmap in pixels
+    int32_t height{ 0 };                     // width of bitmap in pixels
+                                             //       (if positive, bottom-up, with origin in lower left corner)
+                                             //       (if negative, top-down, with origin in upper left corner)
+    uint16_t planes{ 1 };                    // No. of planes for the target device, this is always 1
+    uint16_t bit_count{ 0 };                 // No. of bits per pixel
+    uint32_t compression{ 0 };               // 0 or 3 - uncompressed. THIS PROGRAM CONSIDERS ONLY UNCOMPRESSED BMP images
+    uint32_t size_image{ 0 };                // 0 - for uncompressed images
+    int32_t x_pixels_per_meter{ 0 };
+    int32_t y_pixels_per_meter{ 0 };
+    uint32_t colors_used{ 0 };               // No. color indexes in the color table. Use 0 for the max number of colors allowed by bit_count
+    uint32_t colors_important{ 0 };          // No. of colors used for displaying the bitmap. If 0 all colors are required
+};
+
+struct BMPColorHeader {
+    uint32_t red_mask{ 0x00ff0000 };         // Bit mask for the red channel
+    uint32_t green_mask{ 0x0000ff00 };       // Bit mask for the green channel
+    uint32_t blue_mask{ 0x000000ff };        // Bit mask for the blue channel
+    uint32_t alpha_mask{ 0xff000000 };       // Bit mask for the alpha channel
+    uint32_t color_space_type{ 0x73524742 }; // Default "sRGB" (0x73524742)
+    uint32_t unused[16]{ 0 };                // Unused data for sRGB color space
+};
+#pragma pack(pop)
+
+struct BMP {
+    BMPFileHeader file_header;
+    BMPInfoHeader bmp_info_header;
+    BMPColorHeader bmp_color_header;
+   //const char*
+    BMP(std::string fname) {
+        read(fname);
+    }
+
+    void read(std::string fname) {
+        std::ifstream inp{ fname, std::ios_base::binary };
+        if (inp) {
+            inp.read((char*)&file_header, sizeof(file_header));
+            if (file_header.file_type != 0x4D42) {
+                throw std::runtime_error("Error! File format is not BMP.");
+            }
+            inp.read((char*)&bmp_info_header, sizeof(bmp_info_header));
+
+            // The BMPColorHeader is used only for transparent images
+            /*
+            if (bmp_info_header.bit_count == 32) {
+                // Check if the file has bit mask color information
+                if (bmp_info_header.size >= (sizeof(BMPInfoHeader) + sizeof(BMPColorHeader))) {
+                    inp.read((char*)&bmp_color_header, sizeof(bmp_color_header));
+                    // Check if the pixel data is stored as BGRA and if the color space type is sRGB
+                    check_color_header(bmp_color_header);
+                } else {
+                    std::cerr << "Error! The file \"" << fname << "\" does not seem to contain bit mask information\n";
+                    throw std::runtime_error("Error! Unrecognized file format.");
+                }
+            }
+            */
+            if (bmp_info_header.bit_count != 1) {
+                throw std::runtime_error("Error! Image is not monochrome.");
+            }
+
+            // Jump to the pixel data location
+            inp.seekg(file_header.offset_data, inp.beg);
+
+            // Adjust the header fields for output.
+            // Some editors will put extra info in the image file, we only save the headers and the data.
+            if (bmp_info_header.bit_count == 32) {
+                bmp_info_header.size = sizeof(BMPInfoHeader) + sizeof(BMPColorHeader);
+                file_header.offset_data = sizeof(BMPFileHeader) + sizeof(BMPInfoHeader) + sizeof(BMPColorHeader);
+            } else {
+                bmp_info_header.size = sizeof(BMPInfoHeader);
+                file_header.offset_data = sizeof(BMPFileHeader) + sizeof(BMPInfoHeader);
+            }
+            file_header.file_size = file_header.offset_data;
+
+            if (bmp_info_header.height < 0) {
+                throw std::runtime_error("The program can treat only BMP images with the origin in the bottom left corner!");
+            }
+
+            
+        } else {
+            throw std::runtime_error("Unable to open the input image file.");
+        }
+    }
+
+    int get_width() {
+        return bmp_info_header.width;
+    }
+
+    int get_heigt() {
+        return bmp_info_header.height;
+    }
+
+    int get_bytes_per_raw() {
+        return (bmp_info_header.width + 31) / 32 * 4;
+    }
+
+};
+
+/*  //function to test reading of image
+void test(string filename) {
+    ifstream f(filename, ios::binary);
+    if (!f.good())
+        return;
+
+
+    int height = 39;
+    int width = 120;
+
+    int bpp = 1;
+    int linesize = ((width * bpp + 31) / 32) * 4;
+    int filesize = linesize * height;
+
+    vector<unsigned char> data(filesize);
+
+    //read color table
+    uint32_t color0;
+    uint32_t color1;
+    uint32_t colortable[2];
+    f.seekg(54);
+    f.read((char*)&colortable[0], 4);
+    f.read((char*)&colortable[1], 4);
+    printf("colortable: 0x%06X 0x%06X\n", colortable[0], colortable[1]);
+
+    f.seekg(62);
+    f.read((char*)&data[0], filesize);
+
+    for (int y = height - 1; y >= 0; y--) {
+        for (int x = 0; x < width; x++) {
+            int pos = y * linesize + x / 8;
+            int bit = 1 << (7 - x % 8);
+            int v = (data[pos] & bit) > 0;
+            printf("%d", v);
+        }
+        printf("\n");
+    }
+
+    f.close();
+}*/
---
+++ main.cpp
@@ -0,0 +1,228 @@
+﻿#include <iostream>
+#include <chrono>
+#include <vector>
+#include <Windows.h>
+#include <GL/glut.h>  // GLUT, include glu.h and gl.h
+#include "BMP.h"
+
+//#define DELAY 1
+#define SPEED 20.f
+
+float distance = -100.f;
+
+int WIDTH = 120;
+int HEIGHT = 39;
+
+int winWidth = 1900, winHeight = 980;
+
+float grid_size;
+
+static int frame;
+int FPS = 0;
+long int elapsed_time = 0;
+
+std::vector<std::vector<bool>> field(WIDTH + 1, std::vector<bool>(HEIGHT + 1));
+std::vector<std::vector<bool>> new_field(WIDTH + 1, std::vector<bool>(HEIGHT + 1));
+
+char cell_symbol = '#';
+char empty_cell_symbol = ' ';
+
+std::string name = "Debug/img.bmp";
+std::string finish_code;
+
+std::chrono::high_resolution_clock::time_point timer_start, timer_end, step_time;
+
+float map(float x, float in_min, float in_max, float out_min, float out_max) {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+float crdX(float coord) {
+	return map(coord, 0, winWidth, -1, 1);
+}
+
+float crdY(float coord) {
+	return map(coord, 0, winHeight, -1, 1);
+}
+
+void glPoint(float x, float y) {
+	glVertex3f(crdX(x), -crdY(y), -1);
+}
+
+void changeSize(int w, int h) {
+	winWidth = w;
+	winHeight = h;
+	glViewport(0, 0, w, h);
+	/*
+	// предотвращение деления на ноль
+	if (h == 0)
+		h = 1;
+	float ratio = w * 1.0 / h;
+	// используем матрицу проекции
+	glMatrixMode(GL_PROJECTION);
+	// обнуляем матрицу
+	glLoadIdentity();
+	// установить параметры вьюпорта
+	glViewport(0, 0, w, h);
+	// установить корректную перспективу
+	gluPerspective(45.0f, ratio, 0.1f, 100.0f);
+	// вернуться к матрице проекции
+	glMatrixMode(GL_MODELVIEW);
+	*/
+}
+
+void setup() {
+	BMP BMPfield(name);
+	//if (BMPfield.get_heigt() != HEIGHT || BMPfield.get_width() != WIDTH) throw std::runtime_error("Error! Invalid size of BMP image.");
+	WIDTH = BMPfield.get_width();
+	HEIGHT = BMPfield.get_heigt();
+	int line_size = BMPfield.get_bytes_per_raw();
+	int file_size = line_size * HEIGHT;
+
+	std::ifstream img{ name, std::ios_base::binary };
+
+	if (!img.good()) throw std::runtime_error("Error! Couldn't open the file.");
+
+	img.seekg(0x3e); // 62 bytes, start of pixels info
+
+	std::vector<unsigned char> img_data(file_size);
+
+	img.read((char*)&img_data[0], file_size);
+	img.close();
+
+	for (int y = HEIGHT - 1; y >= 0; y--) {
+		for (int x = 0; x < WIDTH; x++) {
+			int pos = y * line_size + x / 8; // currently readable byte
+			int bit = 1 << (7 - x % 8);
+			//std::cout << "Import " << name << "  size: " << img_data.size() << '\t' << "  byte n:  " << pos << '\n';
+			//std::cout << img_data.size() << '\t' << pos << '\t' << (int)img_data[pos] << '\n'; //1695, 3rd iter - fail
+			int pixel = !((img_data[pos] & bit) > 0); // 1 - black, 0 - white
+			field[x][HEIGHT - y - 1] = pixel;
+		}
+	}
+
+	timer_start = timer_end = step_time = std::chrono::high_resolution_clock::now();
+}
+
+void draw_field() {
+	grid_size = min((float)winWidth / (float)WIDTH, (float)winHeight / (float)HEIGHT);
+
+	for (int y = 0; y < HEIGHT; y++) {
+		glBegin(GL_LINE_STRIP);
+		glPoint(0, y * grid_size);
+		glPoint(WIDTH * grid_size, y * grid_size);
+		glEnd();
+
+		for (int x = 0; x < WIDTH; x++) {
+			glBegin(GL_LINE_STRIP);
+			glPoint(x * grid_size, 0);
+			glPoint(x * grid_size, HEIGHT * grid_size);
+			glEnd();
+
+			if (field[x][y]) {
+				glColor3f(0.5, 0.5, 0.5);
+				glBegin(GL_QUADS);
+				glPoint(((float)x + 0.1) * grid_size, ((float)y + 0.1) * grid_size);
+				glPoint(((float)x + 0.9) * grid_size, ((float)y + 0.1) * grid_size);
+				glPoint(((float)x + 0.9) * grid_size, ((float)y + 0.9) * grid_size);
+				glPoint(((float)x + 0.1) * grid_size, ((float)y + 0.9) * grid_size);
+				glEnd();	
+			}
+			glColor3f(0.15, 0.15, 0.15);
+		}
+	}
+	glBegin(GL_LINE_STRIP);
+	glPoint(WIDTH * grid_size, 0);
+	glPoint(WIDTH * grid_size, HEIGHT * grid_size);
+	glPoint(0, HEIGHT * grid_size);
+	glPoint(WIDTH * grid_size, HEIGHT * grid_size);
+	glEnd();
+}
+// game step
+int field_analyse() {
+	int alive = 0;
+	for (int y = 0; y < HEIGHT; y++) {
+		for (int x = 0; x < WIDTH; x++) {
+			int neighbors = 0; // number of neighbors
+			if (field[(WIDTH + x - 1) % WIDTH][(HEIGHT + y - 1) % HEIGHT] == 1) neighbors++; //  (0, 0)
+			if (field[(WIDTH + x - 1) % WIDTH][y] == 1) neighbors++;						 //  (0, 1)
+			if (field[(WIDTH + x - 1) % WIDTH][(y + 1) % HEIGHT] == 1) neighbors++;			 //  (0, 2)
+			if (field[x][(HEIGHT + y - 1) % HEIGHT] == 1) neighbors++;						 //  (1, 0)
+			if (field[x][(y + 1) % HEIGHT] == 1) neighbors++;								 //  (1, 2)
+			if (field[(x + 1) % WIDTH][(HEIGHT + y - 1) % HEIGHT] == 1) neighbors++;		 //  (2, 0)
+			if (field[(x + 1) % WIDTH][y] == 1) neighbors++;								 //  (2, 1)
+			if (field[(x + 1) % WIDTH][(y + 1) % HEIGHT] == 1) neighbors++;					 //  (2, 2)
+
+			if (field[x][y] == 0 && neighbors == 3) { // newborn
+				new_field[x][y] = 1;
+				alive++;
+			} else if (field[x][y] == 1 && (neighbors == 2 || neighbors == 3)) { // still alive
+				new_field[x][y] = 1;
+				alive++;
+			} else new_field[x][y] = 0; // die
+		}
+	}
+
+	field = new_field;
+
+	return alive;
+}
+
+void renderScene(void) {
+	timer_end = std::chrono::high_resolution_clock::now();
+	static int alive;
+
+	if (std::chrono::duration_cast<std::chrono::microseconds>(timer_end - step_time).count() > 1e6f / SPEED) {
+		 alive = field_analyse();
+		 step_time = timer_end;
+	}
+	
+	if (alive == 0) {
+		finish_code = "Everyone died.";
+		return;
+	}
+
+	// очистка буфера цвета и глубины
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.1, 0.1, 0.1, 1.0);
+	// обнуление трансформации
+	glLoadIdentity();
+	
+	glColor3f(0.15, 0.15, 0.15);
+	glLineWidth(1);
+
+	draw_field();
+
+	glutSwapBuffers();
+
+	frame++;
+
+	elapsed_time = (std::chrono::duration_cast<std::chrono::microseconds>(timer_end - timer_start).count());
+	FPS = 1e6 / elapsed_time;
+	timer_start = timer_end;
+}
+
+int main(int argc, char** argv) {
+
+	// Инициализация GLUT и создание окна
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(10, 80);
+	glutInitWindowSize(winWidth, winHeight);
+	glutCreateWindow("LifeGame");
+
+	// регистрация
+	glutDisplayFunc(renderScene);
+	glutReshapeFunc(changeSize);
+	glutIdleFunc(renderScene);
+
+	if (argc > 1)name = argv[1];
+	setup();
+
+	std::vector<std::vector<bool>> field(WIDTH + 1, std::vector<bool>(HEIGHT + 1));
+	std::vector<std::vector<bool>> new_field(WIDTH + 1, std::vector<bool>(HEIGHT + 1));
+
+	// основной цикл
+	glutMainLoop();
+
+	return 1;
+}
